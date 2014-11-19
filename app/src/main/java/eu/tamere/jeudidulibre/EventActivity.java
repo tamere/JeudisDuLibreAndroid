afb60eee67725d7f2140be64755c19b4919fef7d
Francois Stephany <tulipe.moutarde@gmail.com>
2014-11-19 15:28:51 +0100

    Share button

--- app/src/main/java/eu/tamere/jeudidulibre/EventActivity.java
+++ app/src/main/java/eu/tamere/jeudidulibre/EventActivity.java
@@ -1,10 +1,13 @@
 package eu.tamere.jeudidulibre;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 
 public class EventActivity extends Activity {
     public final static String EVENT = "serialized-event";
@@ -32,5 +35,36 @@ public class EventActivity extends Activity {
         eventTitle.setText(event.title);
         eventDescription.setText(event.description);
         eventSpeaker.setText(event.speaker);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_event, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent parentIntent = getParentActivityIntent();
+                navigateUpTo(parentIntent);
+                return true;
+            case R.id.action_share:
+                this.share();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void share() {
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, event.title);
+        intent.putExtra(Intent.EXTRA_TEXT, event.description);
+
+        startActivity(Intent.createChooser(intent, "Partager"));
     }
 }
